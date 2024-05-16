abec1ce2a48a3104a4156a287404e7ef77e2ddff
davidvilas <davilasanvilar@gmail.com>
2024-05-17 01:21:31 +0200

    Refactored errors in jwtfilter

--- src/main/java/com/viladev/fundshare/auth/JwtFilter.java
+++ src/main/java/com/viladev/fundshare/auth/JwtFilter.java
@@ -4,10 +4,15 @@ import java.io.IOException;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.viladev.fundshare.utils.ApiResponse;
+import com.viladev.fundshare.utils.CodeErrors;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -31,24 +36,38 @@ public class JwtFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         log.info("Request: " + request.getRequestURI());
+        ObjectMapper objectMapper = new ObjectMapper();
         if (!request.getRequestURI().matches("/api/public/.*")) {
             if (request.getCookies() == null) {
-                response.setStatus(401);
+                ApiResponse<Void> apiResponse = new ApiResponse<>(CodeErrors.NOT_JWT_TOKEN, "Not cookies present");
+                response.setStatus(HttpStatus.UNAUTHORIZED.value());
+                response.getWriter().write(objectMapper.writeValueAsString(apiResponse));
                 return;
             }
             Cookie jwtCookie = Arrays.stream(request.getCookies())
                     .filter(cookie -> cookie.getName().equals("JWT_TOKEN"))
                     .findFirst().orElse(null);
             String csrf = request.getHeader("X-API-CSRF");
-            if (jwtCookie == null || csrf == null) {
-                response.setStatus(401);
+            if (jwtCookie == null) {
+                ApiResponse<Void> apiResponse = new ApiResponse<>(CodeErrors.NOT_JWT_TOKEN, "Not JWT token present");
+                response.setStatus(HttpStatus.UNAUTHORIZED.value());
+                response.getWriter().write(objectMapper.writeValueAsString(apiResponse));
+                return;
+            }
+            if (csrf == null) {
+                ApiResponse<Void> apiResponse = new ApiResponse<>(CodeErrors.NOT_CSRF_TOKEN, "Not CSRF token present");
+                response.setStatus(HttpStatus.UNAUTHORIZED.value());
+                response.getWriter().write(objectMapper.writeValueAsString(apiResponse));
                 return;
             }
             Authentication authToken = jwtUtils.validateToken(jwtCookie.getValue(), csrf);
             if (authToken == null) {
-                response.setStatus(401);
+                ApiResponse<Void> apiResponse = new ApiResponse<>(CodeErrors.INVALID_TOKEN, "Invalid jwt token");
+                response.setStatus(HttpStatus.UNAUTHORIZED.value());
+                response.getWriter().write(objectMapper.writeValueAsString(apiResponse));
                 return;
             }
+
             // Set the Authentication object to the SecurityContextHolder
             SecurityContextHolder.getContext().setAuthentication(authToken);
             request.authenticate(response);
--- src/main/java/com/viladev/fundshare/utils/CodeErrors.java
+++ src/main/java/com/viladev/fundshare/utils/CodeErrors.java
@@ -5,10 +5,14 @@ public class CodeErrors {
     private CodeErrors() {
     }
 
+    public static final String NOT_JWT_TOKEN = "NOT_JWT_TOKEN";
+    public static final String NOT_CSRF_TOKEN = "NOT_CSR_TOKEN";
+    public static final String INVALID_TOKEN = "INVALID_TOKEN";
+    public static final String NOT_VALIDATED_ACCOUNT = "NOT_VALIDATED_ACCOUNT";
+
     public static final String USERNAME_ALREADY_IN_USE = "USERNAME_ALREADY_IN_USE";
     public static final String EMAIL_ALREADY_IN_USE = "EMAIL_ALREADY_IN_USE";
     public static final String INVALID_CREDENTIALS = "INVALID_CREDENTIALS";
-    public static final String NOT_VALIDATED_ACCOUNT = "NOT_VALIDATED_ACCOUNT";
     public static final String EXPIRED_VALIDATION_CODE = "EXPIRED_VALIDATION_CODE";
     public static final String INCORRECT_VALIDATION_CODE = "INCORRECT_VALIDATION_CODE";
     public static final String ALREADY_USED_VALIDATION_CODE = "ALREADY_USED_VALIDATION_CODE";
